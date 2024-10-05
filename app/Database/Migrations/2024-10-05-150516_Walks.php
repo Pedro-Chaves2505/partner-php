387da9f0d137f77d7384a74a4d40881140e10cdc
Pedro-Chaves2505 <pedrochaves_10@live.com>
2024-10-05 12:21:54 -0300

    feat: :card_file_box: create table walks

---
+++ app/Database/Migrations/2024-10-05-150516_Walks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Walks extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'starting-point' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => 70,
+            ]
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('walks');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('walks');
+    }
+}
